c9a4c956bbc99991cd4dae827b7feef35611553a
Liviu Ionescu <ilg@livius.net>
2020-03-30 11:47:16 +0300

    f407-disc-blink-tutorial: better control on loops

--- f407-disc-blink-tutorial/src/main.cpp
+++ f407-disc-blink-tutorial/src/main.cpp
@@ -188,17 +188,18 @@ main(int argc, char* argv[])
 
 #define LOOP_COUNT (1 << (sizeof(blinkLeds) / sizeof(blinkLeds[0])))
 
-  int loops = LOOP_COUNT > 2 ? LOOP_COUNT : (5);
+  size_t loops = LOOP_COUNT > 2 ? LOOP_COUNT : (5);
   if (argc > 1)
     {
       // If defined, get the number of loops from the command line,
       // configurable via semihosting.
       loops = atoi (argv[1]);
-      if (loops < LOOP_COUNT)
+      if (loops < (sizeof(blinkLeds) / sizeof(blinkLeds[0])))
         {
-          loops = LOOP_COUNT;
+          loops = (sizeof(blinkLeds) / sizeof(blinkLeds[0]));
         }
     }
+  trace_printf ("Loops: %d\n", loops);
 
   for (size_t i = 0; i < (sizeof(blinkLeds) / sizeof(blinkLeds[0])); ++i)
     {
@@ -234,7 +235,7 @@ main(int argc, char* argv[])
         }
 
       // Blink binary.
-      for (int i = 0; i < loops; i++)
+      for (size_t i = 0; i < loops; i++)
         {
           for (size_t l = 0; l < (sizeof(blinkLeds) / sizeof(blinkLeds[0]));
               ++l)
@@ -248,12 +249,12 @@ main(int argc, char* argv[])
           timer.sleep (Timer::FREQUENCY_HZ);
 
           ++seconds;
-          trace_printf ("Second %u\n", seconds);
+          trace_printf ("Second %u, %u to go\n", seconds, loops - i - 1);
         }
     }
   else
     {
-      for (int i = 0; i < loops; i++)
+      for (size_t i = 0; i < loops; i++)
         {
           blinkLeds[0].turnOn ();
           timer.sleep (BLINK_ON_TICKS);
@@ -262,7 +263,7 @@ main(int argc, char* argv[])
           timer.sleep (BLINK_OFF_TICKS);
 
           ++seconds;
-          trace_printf ("Second %u\n", seconds);
+          trace_printf ("Second %u, %u to go\n", seconds, loops - i - 1);
         }
     }
 
